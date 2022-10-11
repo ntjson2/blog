290002540962de5480b5305d69001f070ccd0aa8
ntjson2 <ntjson@gmail.com>
2022-10-10 20:28:40 -0600

    forms and user accounts

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')), #order matters here. Must be below the one above
     path('', include('blog.urls')),
 ]
